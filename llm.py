2604557a9012f967202a503429b06b4b9ed39f95
Keqing <93966205+chelsea2002@users.noreply.github.com>
2025-07-19 19:19:42 +0800

    Add files via upload

---
+++ llm.py
@@ -0,0 +1,26 @@
+from openai import OpenAI
+from arg import main_args
+from retrying import retry
+
+client = OpenAI()
+
+args = main_args()
+@retry(stop_max_attempt_number=3, wait_fixed=3000)
+def LLM_generation(instruct,prompt):
+
+  response = client.chat.completions.create(
+    model = args.gpt,
+    messages= [
+      {
+          "role": "system",
+            "content": instruct,
+      },
+      {
+          "role": "user",
+          "content": prompt,
+      }
+    ]
+  )
+  print(response)
+  return response.choices[0].message.content
+
