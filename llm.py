94f1e5d3f235c7c7917279e5143fa2e029d57c9a
Keqing <93966205+chelsea2002@users.noreply.github.com>
2025-07-25 13:41:00 +0800

    Update llm.py

--- llm.py
+++ llm.py
@@ -3,24 +3,19 @@ from arg import main_args
 from retrying import retry
 
 client = OpenAI()
-
 args = main_args()
-@retry(stop_max_attempt_number=3, wait_fixed=3000)
-def LLM_generation(instruct,prompt):
 
-  response = client.chat.completions.create(
-    model = args.gpt,
-    messages= [
-      {
-          "role": "system",
-            "content": instruct,
-      },
-      {
-          "role": "user",
-          "content": prompt,
-      }
-    ]
-  )
-  print(response)
-  return response.choices[0].message.content
+@retry(stop_max_attempt_number=3, wait_fixed=3000)
+def LLM_generation(instruct, prompt, n=args.n):  
+    response = client.chat.completions.create(
+        model=args.gpt,
+        messages=[
+            {"role": "system", "content": instruct},
+            {"role": "user", "content": prompt}
+        ],
+        n=n,
+    )
+    results = [choice.message.content for choice in response.choices]
+    return results
+
 
